f749c4cd274441ef3ff1f0a74463c8b05b62a8b3
Wilberth Gamboa <50465353+WilberthGamboa@users.noreply.github.com>
2024-07-17 22:53:44 -0600

    change to ts

--- src/responsiveJS.ts
+++ src/responsiveJS.ts
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const element = document.querySelector('.p-datatable-wrapper');
+    const element:any = document.querySelector('.p-datatable-wrapper');
 
     if (element) {
         
@@ -8,7 +8,7 @@ window.onload = function() {
         console.error('Elemento no encontrado');
     }
 
-    const tableElement = document.querySelector('.p-datatable-table');
+    const tableElement:any = document.querySelector('.p-datatable-table');
 
     if (tableElement) {
         tableElement.style.minWidth = ''; // Elimina la propiedad min-width
