876404d01c32b0476e7e329cb11c78315a367f57
Wilberth Gamboa <50465353+WilberthGamboa@users.noreply.github.com>
2024-07-17 22:59:13 -0600

    responsive solved

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,29 @@ function App() {
       
     }, [isGeolocationEnabled])
    
+    useEffect(() => {
+      window.onload = function() {
+        const element:any = document.querySelector('.p-datatable-wrapper');
+    
+        if (element) {
+            
+            element.style.overflow = 'hidden';
+        } else {
+            console.error('Elemento no encontrado');
+        }
+    
+        const tableElement:any = document.querySelector('.p-datatable-table');
+    
+        if (tableElement) {
+            tableElement.style.minWidth = ''; // Elimina la propiedad min-width
+        } else {
+            console.error('Elemento de tabla no encontrado');
+        }
+    
+    };
+    
+      
+    }, [])
     
     const msgs = useRef<Messages>(null);
 
